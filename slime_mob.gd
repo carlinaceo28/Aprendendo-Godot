ba8bcdb5f383d51f4dfecda262fc11768163dd32
Carlos Alberto Neto <carlosalbertoneto28@gmail.com>
2025-02-22 20:12:56 -0300

    :sparkles: feat: Player Gun criada

--- slime_mob.gd
+++ slime_mob.gd
@@ -5,5 +5,5 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 350.0
+	velocity = direction * 320.0
 	move_and_slide()
